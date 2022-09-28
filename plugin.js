fe39ebf83a62c24d24ac9707c537e8a8db8aa34a
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-28 21:55:48 +0530

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -214,12 +214,7 @@ const reply = (teks) => {
             if (!m.key.fromMe) return
         }
 
-        //Push Message To Console && Auto Read\\
-        if (m.message) {
-            Leo.sendReadReceipt(m.chat, m.sender, [m.key.id])
-            console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
-        }
-	
+
 	//reset limit every 12 hours\\
         let cron = require('node-cron')
         cron.schedule('00 12 * * *', () => {
